952f642e4b0c2ae9473186e94e1ac41cca0a9afa
Abozaid Ibrahim <abozaid.ibrahim11@gmail.com>
2024-01-25 22:38:01 +0200

    Initial Commit

---
+++ Questionare/QuestionareApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionareApp.swift
+//  Questionare
+//
+//  Created by abuzeid on 25.01.24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
